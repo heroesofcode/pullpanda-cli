62a1f3124f0f0e18c676cf7b6d611b4824f72dd7
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2024-06-03 00:04:59 -0300

    Improvement report

--- pullpanda.py
+++ pullpanda.py
@@ -1,8 +1,9 @@
 import git
 import requests
 import os
+import json
 
-GEMINI_AI_TOKEN = ""
+GEMINI_AI_TOKEN = "AIzaSyA3Ilt0P2jPuADTcIWzUvIKxZC6-P9jS6Q"
 
 def get_diff(repo_path):
     """
@@ -42,7 +43,30 @@ def send_diff_for_code_review(diff):
             {
                 "parts": [
                     {
-                        "text": "Me de sugestoes de melhoria pra cada uma das diffs, com exemplo de codigo se for pertinente, como se fosse um code review bem completo: " + diff
+                        "text": """Faça um code review focando dos diffs e siga esse exemplo: 
+
+                            ## Rank: S (Atribua uma nota que vai de S a F, sendo o S o maior e o F o menor)
+
+                            ## Summary 
+
+                                * Criação da feature de login
+                                * Criação da feature de ajuda
+                                * Criação dos testes unitarios
+
+                            ## Code Review 
+
+                            **Diff:** /src/build.gradle.kts
+
+                            **Sugestão:** Escreva aqui a sugestao de alteração
+
+                            **Codigo Formatado:** 
+
+                            ```kotlin
+                                fun main() {
+                                    //exemplo codigo formatado sem elemento de diffs
+                                }
+                            ```
+                        """ + diff
                     }
                 ]
             }
@@ -61,6 +85,56 @@ def send_diff_for_code_review(diff):
             # Get the Markdown text from the response
             markdown_text = response_json['candidates'][0]['content']['parts'][0]['text']
 
+            # Replace backticks (`) with escape characters (\`)
+            markdown_text = markdown_text.replace("`", "\\`").replace("$", "\\$")
+
+            # Generate HTML content
+            html_content = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Render Markdown</title>
+  <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+  <style>
+    body {{
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+      margin: 20px;
+    }}
+    pre {{
+      background-color: #f4f4f4;
+      padding: 10px;
+      border: 1px solid #ddd;
+      overflow-x: auto;
+    }}
+    code {{
+      font-family: 'Courier New', Courier, monospace;
+      background-color: #f4f4f4;
+      padding: 2px 4px;
+      border-radius: 4px;
+    }}
+    #logo {{
+      display: block;
+      margin: 0 auto;
+      width: 200px;
+    }}
+  </style>
+</head>
+<body>
+  <img id="logo" src="../resources/pullpanda-logo.png" alt="Logo">
+  <div id="content"></div>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {{
+      const markdownText = `{markdown_text}`;
+      const content = document.getElementById('content');
+      content.innerHTML = marked.parse(markdownText);
+    }});
+  </script>
+</body>
+</html>"""
+
             # Get the directory where the script is being executed
             script_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -72,17 +146,17 @@ def send_diff_for_code_review(diff):
                 os.makedirs(save_directory)
 
             # Name of the file to be saved
-            file_name = "code_reviewed.md"
+            file_name = "code_reviewed.html"
 
             # Full path to the file to be saved
             file_path = os.path.join(save_directory, file_name)
 
-            # Save the Markdown content to a file
+            # Save the HTML content to a file
             with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(markdown_text)
+                file.write(html_content)
 
             # Print the path of the saved file for the user
-            print("The Markdown file has been saved at:", file_path)
+            print("The HTML file has been saved at:", file_path)
         else:
             print("Unable to find the 'text' parameter in the JSON response.")
     else:
@@ -93,35 +167,60 @@ if __name__ == "__main__":
     print(
         """""
 
-
-
-                                                           ⣠⣾⣿⣿⣿⣿⣿⣿⣆             ⢠⣾⣿⣿⣿⣿⣿⣷⣦⡀
-                                                         ⣰⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠉⠉⠉⠉⠉⠉⠉⠉⠈⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣷
-                                                        ⣿⣿⣿⣿⣿⣿⣿⠟⠁                 ⠈⠙⢿⣿⣿⣿⣿⣿⣿⡇
-                                                        ⢻⣿⣿⣿⣿⡟⠁                      ⠙⣿⣿⣿⣿⣿⠁
-                                                        ⠈⠻⢿⣿⠟     ⣀⣤⣤⣤⡀     ⢀⣤⣤⣤⣄⡀    ⠘⣿⡿⠿⠃
-                                                          ⢠⡟    ⣠⣾⣿⣿⣟⣿⡇     ⢸⣿⣿⣻⣿⣿⣦    ⠸⣧
-                                                          ⣼⠁    ⣿⣿⣿⣿⣿⡟  ⢀⢀⢀  ⢽⣿⣿⣿⣿⣿⡇  ⣀⣤⣿⣷⣴⣶⣦⣀⡀
-                                                        ⢀⣠⣤⣤⣠⣇  ⣿⣿⣿⣿⣿   ⢀⢀⢀  ⢹⣿⣿⣿⣿⡇ ⣿⣿⣿⡏⢹⣿⠉⣿⣿⣿⣷
-                                                    ⢠⣾⣿⣿⣿⣿⣿⣿⣿⣶⣄ ⠹⣿⣿⠿⠋         ⠈⠻⣿⣿⠟ ⢸⣿⣇⣽⣿⠿⠿⠿⣿⣅⣽⣿⡇
-                                                    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆                     ⠈⣿⣿⣟⠁   ⠈⣿⣿⣿⡇ 
-                                                ⠛⠛⠛⠛⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛
-                                                    ⠘⠛⠻⢿⣿⣿⣿⣿⣿⠟⠛⠁                                       
-
-
-                            :::::::::  :::    ::: :::        :::             :::::::::     :::     ::::    ::: :::::::::      :::     
-                            :+:    :+: :+:    :+: :+:        :+:             :+:    :+:  :+: :+:   :+:+:   :+: :+:    :+:   :+: :+:   
-                            +:+    +:+ +:+    +:+ +:+        +:+             +:+    +:+ +:+   +:+  :+:+:+  +:+ +:+    +:+  +:+   +:+  
-                            +#++:++#+  +#+    +:+ +#+        +#+             +#++:++#+ +#++:++#++: +#+ +:+ +#+ +#+    +:+ +#++:++#++: 
-                            +#+        +#+    +#+ +#+        +#+             +#+       +#+     +#+ +#+  +#+#+# +#+    +#+ +#+     +#+ 
-                            #+#        #+#    #+# #+#        #+#             #+#       #+#     #+# #+#   #+#+# #+#    #+# #+#     #+# 
-                            ###         ########  ########## ##########      ###       ###     ### ###    #### #########  ###     ### 
-
-                                                                                                                  ~ by Heroes of Code
-
-
-
-                                                                                                                  
+                                                                                                                    
+                                                                                                    
+                                                                                                    
+                            ..-****=.                                                               
+                          .:##=....-#%-.        .:                                                  
+                         .**.        .##- ....#@@%*--===+++==-::-#%%#=.                             
+                        .==.          .*#*%@@@@@@@@@@@@@@@@@%#-@*.. ..=-.                           
+         :=#%%%%%%%%%%%%=%:            -@@@@@@@@@@@@@@%#***+:.+*.      .=%%%%%%%*-.                 
+        +@@%***@=#=@@@@@+#:            -@@@@@@@@@@@@@@#*****=:%*..      .@@@@@@@@@%-                
+       -%@@#+*#@+*=@@@@@%-=.       ...=@@@@@@@@@@@@@@@@#*+=:+@@@@+..   ..@@@@@@@@@@+                
+       -%@@@@@@@@@@@@@@@@#++.     .*@@@@@@@@@@@%-+%@@@@@@@%%@@@@@@@-.  .#@@@@@@@@@@+                
+       =%-:..::::::::.......=:. .=@@@@@@@@@@@@@@@#*%@@@@@@@@@@@@#%@@=. .::::::::::++                
+       +%-:%+%@@@@@@@-::::.:@*==%@@@@@@@@@@@%##%%@@@@@@@@@@@@@@@+=@@@=:.##*@%:::::++                
+       #@-:#%+:-%%=:%%%+.:.@%**@@@@@@@@@-:...   ..:+@@@@@@@@@@@@@@@@@%.%%:::%%+:::++                
+       %@-::++++++++-=++-.=@**%@@@@@@@-.            .#@@@@@@@@*....=@@:=++++++++=:+*                
+       %@-:::::::------:..=**#@@@@@@@:.       ...    .@@@@@@#.     .=@*:::::::::::+#                
+       %@-::::-::::-::::.*@@@@@@@@@@:.     .=%@@@%:. .%@@@@%.....   :@#:-::---:.::+%                
+       %@-::::::==::=-:.=@@@@@@@@@@*.     .#@@@@@@@=  %@@@%.-@@@+.  :@#:-:-----.-:+%                
+       %@-:::::::-***+::.%#@@@@@@@@=......-@@@#:::*=.:@@@@=:%@@@*.  :@%-:+*****.=:+%                
+       %@-:#-:::=%%%.%%%:.*#%@@@@@@=...:--:@@@=.  ...*@@@@-:.:%@+.. :@@=#%#:-%%-+:+@                
+       %@-::::-%@@%:+%::..*%**@@@@@+...::==:#@@*-+.-@@@@@@%::*@%:...:@@+:@@@+*@@*:+@                
+       %@-:##########.:#=.:@#*#@@@@@-....:---:...*@@@@@#---#++-=-:..:@@+*##-:::.+:+@                
+       %@-::-=++=-:::..:-*#:%#*#@@@@@#:........=@@@@@. :*###=%::...:@@@+-===-::.=:+@                
+       %@-:::........:%@%:..-@%**@@@@@@@@@%@@@@@@@@@@%:    .+@=...=@@@@::::::::.=:+@                
+       %@-:::......=@@*:.   ..*@#*#@@@@@@@@@@@@@@@@@@@@%:+*@@@@@@@@@@@-+********=:+@                
+       %@-::....=@@@%#%#-.    ..%%**#@@@@@@@@@@@@@@@@@@*-@@@@@@@@@@@@=..........-:+@                
+       %@-.%=.%@@=:.. ..-#-.    ..%@**#%@@@@@@@@@@@@@@@@@@@@@@@@@%%*:.::::::::::::+@                
+       %@-..*@%=..       .:*:.    ..=#%%%%@@@@@@@@@@@@@@@@@@@@%%%*.-=....:*@%+:.-:+@                
+       #@-:@@+:.           .=.        ..#@@@@@@@@@@@@@@@%%%#%@+..   .%.#:-*%+%+.=:+@                
+       =#:%%-..         .-. .-:.         ...--+#@@@@@@@#=--...       .**.:+@@=:.+:+@                
+       ::%#:.            .=- .-.                 .   .          ....  .+@:=%%%%:*:+@                
+       .*#:.               :+.-:       ..   .   .. .    .       ..=.   .-%:%@@%.*:+%                
+       -#:.                 :#.+.    -@@@@*.#@..@%:@*. =@..    .:+.     .=%:-++.=:+%                
+       +=.               :::..*#.    =@*.=@=#@..@%:@#. =@..    .*:.      .=%:.::=:+%                
+      :#:.                ..===%.    =@@@@=:%@..@%:@#..=@..    .*.....    .*+.-*#:++                
+      =+.                    .*@.    -@*.. .=@@@%::%@%*-###+....-.-....:-=++*.:...--      .         
+      #=:..-*#%@@@@@@@%#+:.:...#..  .=##*..:%:..**.*#-@@+:#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*.       
+     .#%*+==-:.........::-=+%%*+-.  .%@-%@+@@@-.@@%#@=@=:@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%-       
+      .*.          .:*#####%%%++-.  .%@@@#%@#@#.@%@@@=@-#*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#        
+      .*.          ...      ...-%*...%@..*@+-%@:@#-@%+@+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-        
+       #.                       .:*#+-...... ..... ....=%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*.        
+       *-                      ....:=*%@@@@%+:..      .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=         
+       -+.                      ............:-*@+.   .:@@@@@@@@@@@@@@@@@-..:@@@@@@@@@@@@@%+         
+       :#-.                                    .-%*. .@%@@@@@@@@@@@@@@@*. .-@@@@@@@@@@@@@#          
+       .-%:                                   .. .-%.=%@@@@@@@@@@@@@@@@@%#@@@@@@@@@@@@@@@*          
+       :-=#:.                                 .-*:.**+%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#           
+       -*:+%:.                           ..:..  -#.::%%%%%%%%%%@@@@@@@@@@@@@@@@@@@%%%%%%+           
+       -#:.:#+..                           .-#. .=..-%#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#            
+       -*:. .:+-.                  .:-+##%#*****###-@#######%%%%%%%%%%%%%%%%%%%%%%%%%##*            
+      %++++*##%%%%%%%%%%%%%%%%%%%%%%-*#%%%*+=*%#+-=+##################################%:-           
+      %%@@@@@@@@@@@@@@@@@@@@@@@@@@@%#*+=---:-+*=#=*************#######%%###*+++==----..--=          
+                                        %*#+--. ::=:-*+===-:.............    ...                    
+                                                                                                    
+                                                                       ~ by Heroes of Code
     """
     )
 
